10c2ba0b7141bd697a13a709fc946c70a15d9dda
fff <3032351083@qq.com>
2024-04-06 20:48:51 +0800

    首次提交

---
+++ src/main/java/com/example/LabManageSpring/LabManageSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.LabManageSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManageSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabManageSpringApplication.class, args);
+	}
+
+}
